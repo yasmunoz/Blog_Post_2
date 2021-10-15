63326a1563721fb4512ff4ad6ee7474b473e5dae
Phil Chodrow <pchodrow@gmail.com>
2021-10-15 12:40:53 -0700

    updates to prep for monday

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 '''
-In this example, we are going to look at creating, serving, and deploying a *very simple* webapp. In the following several lectures, we'll see how to add some interesting interactivity to our app. 
+In this example, we are going to look at creating, serving, and deploying a *very simple* webapp. In the following several lecture, we'll see how to add some interesting interactivity to our app. 
 
 # Prerequisites
 
@@ -20,7 +20,7 @@ export FLASK_ENV=development; flask run
 
 # Deployment
 
-*Note*: these notes are written for the version of the app that Phil is using, which is indeed called pic16b-minimal-demo. In order to make your own version, you would need to give the app a different name (because one with this name already exists now).
+*Note*: these notes are written for the version of the app that Phil is using, which is indeed called pic16b-minimal-demo. In order to make your own version, you would need to give the app a different name (because one with this name already exists on the internet now).
 
 Sign up for Heroku, create app called pic16b-minimal-demo
 
@@ -39,16 +39,16 @@ https://pic16b-minimal-demo.herokuapp.com
     
 # Sources
 
-This set of lecture notes is based in part on previous materials developed by Erin George (UCLA Mathematics) and the tutorial [here](https://stackabuse.com/deploying-a-flask-application-to-heroku/). 
+This set of lecture notes is based in part on previous materials developed by [Erin George](https://www.math.ucla.edu/~egeo/) (UCLA Mathematics) and the tutorial [here](https://stackabuse.com/deploying-a-flask-application-to-heroku/). 
 '''
 
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-# @app.route("/")
+@app.route("/")
 # simplest possible approach
-# def main():
-#     return "hi there!"
+def main():
+    return "hi there!"
 
 
 # slightly less trivial
@@ -57,22 +57,22 @@ app = Flask(__name__)
 
 # A little fancier
 
-@app.route("/")
-def main():
-    return render_template("main_better.html")
+# @app.route("/")
+# def main():
+#     return render_template("main_better.html")
 
 # getting basic user data
-@app.route('/ask/', methods=['POST', 'GET'])
-def ask():
-    if request.method == 'GET':
-        return render_template('ask.html')
-    else:
-        try:
-            return render_template('ask.html', name=request.form['name'], student=request.form['student'])
-        except:
-            return render_template('ask.html')
+# @app.route('/ask/', methods=['POST', 'GET'])
+# def ask():
+#     if request.method == 'GET':
+#         return render_template('ask.html')
+#     else:
+#         try:
+#             return render_template('ask.html', name=request.form['name'], student=request.form['student'])
+#         except:
+#             return render_template('ask.html')
 
-# 
-@app.route('/profile/<name>/')
-def hello_name(name):
-    return render_template('profile.html', name=name)
+# # 
+# @app.route('/profile/<name>/')
+# def hello_name(name):
+#     return render_template('profile.html', name=name)
--- lecture-outline.md
+++ lecture-outline.md
@@ -1,8 +1,15 @@
+# Lecture 1
+
 1. Big picture: why webapps with Flask?
-2. An initial app: single template
-    - Running Flask
+2. An initial app
+    - Minimal Python components
+    - Running Flask from the command line
+    - Adding simple templates from the `templates` directory. 
 3. A more complex app
     - Multiple pages
-    - Overview of how the pages work
-    - Passing information
+    - Basic structure of jinja pages
+    - Passing information via arguments to `render_template`
 4. Deployment to Heroku
+    - requirements.txt
+    - Procfile
+    - heroku commands
