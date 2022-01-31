d22cc274ffe7e3e2602aa750f9b3a5b41f1ec48f
yasmunoz <91529788+yasmunoz@users.noreply.github.com>
2022-01-30 17:25:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,19 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # simplest possible approach
+@app.route("/")
 def main():
     return render_template("main.html")
+
+@app.route('/ask/', methods=['POST', 'GET'])
+def ask():
+    if request.method == 'GET':
+        return render_template('ask.html')
+    else:
+        try:
+            return render_template('ask.html', name=request.form['name'], student=request.form['student'])
+        except:
+            return render_template('ask.html')
+@app.route('/profile/<name>/')
+def hello_name(name):
+    return render_template('profile.html', name=name)
