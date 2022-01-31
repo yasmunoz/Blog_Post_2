7c2ee94aa931d8c3dd1a46e195e8575f314425e5
yasmunoz <91529788+yasmunoz@users.noreply.github.com>
2022-01-30 23:04:22 -0800

    Update app.py

--- app.py
+++ app.py
@@ -51,10 +51,4 @@ def main():
 
 @app.route('/ask/', methods=['POST', 'GET'])
 def ask():
-    if request.method == 'GET':
-        return render_template('ask.html')
-    else:
-        try:
-            return render_template('ask.html', name=request.form['name'], student=request.form['student'])
-        except:
-            return render_template('ask.html')
+    return render_template('ask.html')
